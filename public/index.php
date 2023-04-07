0bc9ebe004d815922b27b01d01092b31f40d6edc
Guilherme Amaral <gui.amaral123@outlook.com>
2023-04-07 19:17:15 +0000

    Created Folder Architecture

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
-<?php
-
-echo "Hello, world!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/main.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
