93531a4d5d17229f0322c519eebb5b25899ac67d
Guilherme Amaral <gui.amaral123@outlook.com>
2023-04-08 14:27:59 +0000

    create: inserir

--- src/service/Tarefa.service.php
+++ src/service/Tarefa.service.php
@@ -2,9 +2,23 @@
 
 class TarefaService
 {
-    public function inserir()
+    private $conexao;
+    private $tarefa;
+
+    public function __construct(Conexao $conexao, Tarefa $tarefa)
     {
+        $this->conexao = $conexao->conectar();
+        $this->tarefa = $tarefa;    
     }
+
+    public function inserir(): void
+    {
+        $query = 'INSERT INTO tb_tarefas(tarefa) VALUES (:tarefa);';
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+        $stmt->execute();
+    }
+
     public function recuperar()
     {
     }
