313a10b3631e8ab23e799663e3109dd34ff34946
Guilherme Amaral <gui.amaral123@outlook.com>
2023-04-08 14:17:16 +0000

    create: editar

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,32 @@
-console.log("Hello, world!");
+function editar(id, txtTarefa) {
+
+    let form = document.createElement('form');
+    form.action = 'index.php?pag=index&acao=atualizar';
+    form.method = 'post';
+    form.className = 'row';
+
+    let inputTarefa = document.createElement('input');
+    inputTarefa.type = 'text';
+    inputTarefa.name = 'tarefa';
+    inputTarefa.className = 'col-9 form-control';
+    inputTarefa.value = txtTarefa;
+
+    let inputId = document.createElement('input');
+    inputId.type = 'hidden';
+    inputId.name = 'id';
+    inputId.value = id;
+
+    let button = document.createElement('button');
+    button.type = 'submit';
+    button.className = 'col-3 btn btn-info';
+    button.innerHTML = 'Atualizar';
+
+    form.appendChild(inputTarefa);
+    form.appendChild(inputId);
+    form.appendChild(button);
+
+    let tarefa = document.getElementById(`tarefa_${id}`);
+    tarefa.innerHTML = '';
+    tarefa.insertBefore(form, tarefa[0]);
+    
+}
