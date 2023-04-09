a46b0f9cf3dbb953df94e34aa0f1c804ac63f2b0
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-04-09 12:06:20 -0300

    created: delete

--- src/service/Tarefa.service.php
+++ src/service/Tarefa.service.php
@@ -21,7 +21,7 @@ class TarefaService
 
     public function recuperar(): array|false
     {
-        $query = 'SELECT t.id, s.status, t.tarefa FROM tb_tarefas AS t LEFT JOIN tb_status AS s ON (t.id_status = s.id)';
+        $query = 'SELECT t.id, s.status, t.tarefa FROM tb_tarefas AS t LEFT JOIN tb_status AS s ON (t.id_status = s.id);';
         $stmt = $this->conexao->prepare($query);
         $stmt->execute();
 
@@ -30,15 +30,19 @@ class TarefaService
 
     public function atualizar()
     {
-        $query = "UPDATE tb_tarefas SET tarefa = ? WHERE id = ?";
+        $query = "UPDATE tb_tarefas SET tarefa = ? WHERE id = ?;";
         $stmt = $this->conexao->prepare($query);
         $stmt->bindValue(1, $this->tarefa->__get('tarefa'));
         $stmt->bindValue(2, $this->tarefa->__get('id'));
 
         return $stmt->execute();
     }
-    
+
     public function remover()
     {
+        $query = 'DELETE FROM tb_taferas WHERE id = :id;';
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue(':id', $this->tarefa->__get('id'));
+        $stmt->execute();
     }
 }
