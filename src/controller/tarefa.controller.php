ea120ed8705f69e57ae4ed51fa4d9858baada62a
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-04-10 19:01:52 -0300

    fix: controller

--- src/controller/tarefa.controller.php
+++ src/controller/tarefa.controller.php
@@ -1,15 +1,18 @@
 <?php
 
+namespace src\controllers;
+
+require '../model/Tarefa.model.php';
+require '../database/Conexao.php';
+require '../service/Tarefa.service.php';
+
 use src\database\Conexao;
 use src\model\Tarefa;
 use src\service\TarefaService;
 
-require "../model/Tarefa.model.php";
-require "../service/Tarefa.service.php";
-require "../database/Conexao.php";
-
 $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
-if($acao == 'inserir' ) {
+
+if ($acao == 'inserir') {
     $tarefa = new Tarefa();
     $tarefa->__set('tarefa', $_POST['tarefa']);
 
